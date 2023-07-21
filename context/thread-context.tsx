7b1fe181ff6f2b52f608e544460123477ba165bc
valonmulolli <valonmulolli@gmail.com>
2023-07-22 01:43:42 +0200

    Thread Provider

---
+++ context/thread-context.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Thread } from '../types/threads';
+import { generateThreads } from '../utils/generate-fake-data';
+
+export const ThreadsContext = React.createContext<Thread[]>([]);
+
+export const ThreadProvider = ({
+	children,
+}: React.PropsWithChildren): JSX.Element => {
+	const [threads, setThreads] = React.useState<Thread[]>([]);
+	React.useEffect(() => {
+		setThreads(generateThreads());
+	}, []);
+
+	return (
+		<ThreadsContext.Provider value={threads}>
+			{children}
+		</ThreadsContext.Provider>
+	);
+};
