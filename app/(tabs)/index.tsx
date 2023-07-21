834adcbd2b4956e870b0623d9639bfdc2401c4c1
valonmulolli <valonmulolli@gmail.com>
2023-07-22 00:35:12 +0200

    Generate Fake Data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,37 @@
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import * as React from 'react';
+import { ScrollView, StyleSheet, Platform, RefreshControl } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import Lottie from 'lottie-react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { createRandomUser } from '../../utils/generate-fake-data';
+
+const user = createRandomUser()
+
+console.log(user)
 
 export default function TabOneScreen() {
+	const animationRef = React.useRef<Lottie>(null);
+
 	return (
 		<SafeAreaView>
-			<ScrollView>
+			<ScrollView
+				contentContainerStyle={{
+					paddingHorizontal: 10,
+				}}
+				refreshControl={
+					<RefreshControl
+						refreshing={false}
+						onRefresh={() => {
+							animationRef.current?.play();
+						}}
+						tintColor={'transparent'}
+					/>
+				}
+			>
 				<Lottie
+					ref={animationRef}
 					source={require('../../assets/threads.json')}
-					loop={true}
+					loop={false}
 					autoPlay={true}
 					style={{ width: 90, height: 90, alignSelf: 'center' }}
 				/>
